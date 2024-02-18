825707578a1a985f6784f21ccb6cd2bcf3ccfeeb
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2024-02-18 18:25:41 +0100

    Modify update to cindy 0.1.1

--- cindy.lua
+++ cindy.lua
@@ -1,5 +1,5 @@
 local cindy = {
-	_VERSION     = 'cindy 0.1',
+	_VERSION     = 'cindy 0.1.1',
 	_LICENSE     = 'WTFPL, http://www.wtfpl.net',
 	_URL         = 'https://github.com/megagrump/cindy',
 	_DESCRIPTION = 'True Colors for LÖVE 11',
@@ -7,8 +7,8 @@ local cindy = {
 
 --[[-----------------------------------------------------------------------------------------------------------------
 
-cindy adds functions to LÖVE 11.x that accept/return colors in the [0-255] range instead of the newly introduced
-[0.0-1.0] range.
+cindy adds functions to LÖVE 11.x that accept/return colors in the [0,255] range instead of the newly introduced
+[0.0,1.0] range.
 
 In love.graphics:
 - clearBytes
@@ -53,41 +53,41 @@ local function round(v)
 	return math.floor(v + .5)
 end
 
--- convert a single channel value from [0-1] to [0-255]
+-- convert a single channel value from [0.0,1.0] to [0,255]
 function cindy.channel2byte(c)
 	return round(c * 255)
 end
 
--- convert a single channel value from [0-255] to [0-1]
+-- convert a single channel value from [0,255] to [0.0,1.0]
 function cindy.byte2channel(c)
 	return c / 255
 end
 
--- convert RGBA values from [0-1] to [0-255]
+-- convert RGBA values from [0.0,1.0] to [0,255]
 function cindy.rgba2bytes(r, g, b, a)
 	return round(r * 255), round(g * 255), round(b * 255), a and round(a * 255)
 end
 
--- convert RGBA values from [0-255] to [0-1]
+-- convert RGBA values from [0,255] to [0.0,1.0]
 function cindy.bytes2rgba(r, g, b, a)
 	return r / 255, g / 255, b / 255, a and a / 255
 end
 
--- convert RGBA value table from [0-1] to [0-255]. places the result in dest, if given
+-- convert RGBA value table from [0.0,1.0] to [0,255]. places the result in dest, if given
 function cindy.table2bytes(color, dest)
 	dest = dest or {}
 	dest[1], dest[2], dest[3], dest[4] = cindy.rgba2bytes(color[1], color[2], color[3], color[4])
 	return dest
 end
 
--- convert RGBA value table from [0-255] to [0-1]. places the result in dest, if given
+-- convert RGBA value table from [0,255] to [0.0,1.0]. places the result in dest, if given
 function cindy.bytes2table(color, dest)
 	dest = dest or {}
 	dest[1], dest[2], dest[3], dest[4] = cindy.bytes2rgba(color[1], color[2], color[3], color[4])
 	return dest
 end
 
--- convert RGBA values or table from [0-1] to [0-255]. returns separate values
+-- convert RGBA values or table from [0.0,1.0] to [0,255]. returns separate values
 function cindy.color2bytes(r, g, b, a)
 	if type(r) == 'table' then
 		r, g, b, a = r[1], r[2], r[3], r[4]
@@ -96,7 +96,7 @@ function cindy.color2bytes(r, g, b, a)
 	return cindy.rgba2bytes(r, g, b, a)
 end
 
--- convert RGBA values or table from [0-255] to [0-1]. returns separate values
+-- convert RGBA values or table from [0,255] to [0.0,1.0]. returns separate values
 function cindy.bytes2color(r, g, b, a)
 	if type(r) == 'table' then
 		r, g, b, a = r[1], r[2], r[3], r[4]
@@ -105,7 +105,7 @@ function cindy.bytes2color(r, g, b, a)
 	return cindy.bytes2rgba(r, g, b, a)
 end
 
--- patch all LÖVE functions to accept colors in the [0-255] range
+-- patch all LÖVE functions to accept colors in the [0,255] range
 function cindy.applyPatch()
 	gfx.clear, gfx.getColor, gfx.setColor = gfx.clearBytes, gfx.getColorBytes, gfx.setColorBytes
 	gfx.getBackgroundColor, gfx.setBackgroundColor = gfx.getBackgroundColorBytes, gfx.setBackgroundColorBytes
@@ -149,19 +149,18 @@ end
 
 function gfx.clearBytes(...)
 	local nargs = select('#', ...)
+	if nargs == 0 then return clear() end
 
-	if nargs == 0 or type(select(1, ...)) == 'boolean' then
-		return clear(...)
-	end
+	local argtype = type(select(1, ...))
+	if argtype == 'boolean' then return clear(...) end
 
-	local args = {...}
-	for i = 1, nargs do
-		if type(args[i]) == 'table' then
-			args[i] = cindy.bytes2table(args[i], tempTables[i])
-		elseif type(args[i]) == 'number' then
-			args[i] = args[i] / 255
-		end
-	end
+	local converter = argtype == 'table' and cindy.bytes2table or cindy.byte2channel
+	local args, i = {...}, 1
+
+	repeat
+		args[i] = converter(args[i], tempTables[i])
+		i = i + 1
+	until type(args[i]) ~= argtype
 
 	return clear(unpack(args))
 end
